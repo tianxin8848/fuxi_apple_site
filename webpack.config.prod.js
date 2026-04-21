e43bdcf3e54ec68f5a8320df57900a6fce790fcd
xin <1045360566@qq.com>
2026-04-21 13:14:45 +0800

    fix:redirect page

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
     new CopyPlugin({
       patterns: [
         { from: 'pages', to: 'pages' },
