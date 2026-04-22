b576a703762331254a40163dc589ae80833ed9d5
xin <1045360566@qq.com>
2026-04-22 11:18:20 +0800

    delet

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 3006;
+const port = process.env.PORT || 3007;
 
 const distDir = path.join(__dirname, 'dist');
 // Default to source files for local debugging.
